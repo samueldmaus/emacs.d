aeb374c4f9304272f37e0c775f8c682e477dea7a
Sam Maus <smaus@boldorange.com>
2025-11-11 10:28:29 -0600

    starting my emacs config

---
+++ init.el
@@ -0,0 +1,24 @@
+;; Sam's init file begins here
+
+;; core settings
+(setq
+ make-backup-files nil
+ auto-save-default nil
+ )
+
+;; set up package.el to work with MELPA
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/")
+	     )
+
+;; list of packages to install
+(setq package-selected-packages '()
+      )
+
+;; if packages are not found install them
+(when (cl-find-if-not #'package-installed-p package-selected-packages)
+  (package-refresh-contents)
+  (mapc #'package-install package-selected-packages)
+  )
